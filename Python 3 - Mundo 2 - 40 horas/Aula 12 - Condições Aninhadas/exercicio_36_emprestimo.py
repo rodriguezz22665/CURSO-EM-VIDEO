db7092e0c9ba8f8234e0e9596413e7a466e6f143
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-07-31 12:53:40 -0300

    novos exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 12 - Condições Aninhadas/exercicio_36_emprestimo.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 12 - Condições Aninhadas/exercicio_36_emprestimo.py
@@ -1,12 +1,16 @@
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
 casa = float(input("Digite o valor da casa: R$ "))
 
 salario = float(input("Digite o valor do seu salario: R$ "))
 
 anos = int(input("Em quantos anos deseja pagar esta casa? "))
 
+prestacao = casa/(anos*12)
 
-if casa/(anos*12) > salario * 0.3:
-    print("Orçamento reprovado!")
+if prestacao >= salario * 0.3:
+    print("Valor da casa: {:.2f}\nTempo para quitar: {} anos\nValor das parcelas: R$ {:.2f}".format(casa,anos,prestacao))
+    print("Empréstimo negado!")
 else:
-    print("Orçamento aprovado!")
-    print("Valor da casa: {:2f}\nValor das parcelas: R$ {:2f}".format(casa,(casa/(anos*12))))    
+    print("Empréstimo aprovado!")
+    print("Valor da casa: {:.2f}\nTempo para quitar: {} anos\nValor das parcelas: R$ {:.2f}".format(casa,anos,prestacao)) 
--- Python 3 - Mundo 2 - 40 horas/Aula 12 - Condições Aninhadas/exercicio_37_base_convers.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 12 - Condições Aninhadas/exercicio_37_base_convers.py
@@ -1,3 +1,19 @@
+#Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+
 num = int(input("Digite um número inteiro: "))
 
-opcao = input("Qual será a base de conversão? Escolha uma das opções a seguir: \n- 1 para binário\n- 2 para octal\n- 3 para hexadecimal")
+opcao = int(input('''Qual será a base de conversão? Escolha uma das opções a seguir: 
+              - 1 para binário
+              - 2 para octal
+              - 3 para hexadecimal
+              '''))
+
+if opcao == 1:
+    print("{} convertido para binário é igual a {}".format(num,bin(num)[2:]))
+elif opcao == 2:        
+    print("{} convertido para octal é igual a {}".format(num,oct(num)[2:]))
+elif opcao == 3:
+    print("{} convertido para hexadecimal é igual a {}".format(num,hex(num)[2:]))    
+
+else:
+    print("Número inválido!")
