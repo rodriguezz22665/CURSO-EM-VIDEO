badbe53be7e663b606faa88d9f56965eaf625f00
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-08-02 16:37:27 -0300

    Update exercicio_42_analis_triangulos.py

--- Python 3 - Mundo 2 - 40 horas/Aula 12 - Condições Aninhadas/exercicio_42_analis_triangulos.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 12 - Condições Aninhadas/exercicio_42_analis_triangulos.py
@@ -1,4 +1,26 @@
 #Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
 #– EQUILÁTERO: todos os lados iguais
 #– ISÓSCELES: dois lados iguais, um diferente
-#– ESCALENO: todos os lados diferentes
+#– ESCALENO: todos diferentes
+
+#Desafio 35 
+#Desenvolva um programa que leia o comprimento de três retas, 
+#e diga ao usuário se elas podem ou não formar um triângulo.
+
+reta1 = float(input("Digite o comprimento da primeira reta: "))
+reta2 = float(input("Digite o comprimento da segunda reta: "))
+reta3 = float(input("Digite o comprimento da terceira reta: "))
+
+if reta1 == reta2 and reta1 == reta3 and reta3 == reta2 == 1:
+    tipo = 'Equilátero'
+elif reta1 != reta2 and reta1 != reta3 and reta3 != reta2 != 1: 
+    tipo = 'Escaleno'
+else:
+    tipo = 'Isóceles'    
+
+if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta3 + reta2 > reta1:
+    print("Os seguimentos {}, {} e {} formam um triângulo {}.".format(reta1,reta2,reta3,tipo))
+else:
+    print("Os seguimentos {}, {}, e {} não formam um triângulo!".format(reta1,reta2,reta3))    
+
+
