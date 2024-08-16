086e3ea1c530f0ca4aaa03c8b472297f60293bbb
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-08-16 08:25:59 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_60_calculo_fatorial.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_60_calculo_fatorial.py
@@ -1,3 +1,19 @@
 #Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
 #5! = 5 x 4 x 3 x 2 x 1 = 120
 
+num = int(input("Digite um número para vizualizar o seu fatorial: "))   
+count = num + 1
+fat = []
+
+
+while count >= 2:
+    count -= 1
+    fat.append (int(count))
+   
+  
+    print(count)
+
+print('fim')
+print(fat)  
+fator = fat[0] * fat[1] * fat[2] * fat[3] * fat[4] 
+print(fator)
