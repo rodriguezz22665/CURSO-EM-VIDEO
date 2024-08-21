89b380622b72be1044ff18d4aea84a9764f1c766
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-08-21 14:11:56 -0300

    Novos exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_63_Sequen_Fibonacci_v1.0.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_63_Sequen_Fibonacci_v1.0.py
@@ -1,2 +1,24 @@
 #Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. Exemplo:
 #0 – 1 – 1 – 2 – 3 – 5 – 8
+#"A sequência de Fibonacci é uma sequência numérica infinita em que cada termo a partir do terceiro é a soma dos dois termos anteriores.
+#Portanto, a sequência de Fibonacci é (1,1,2,3,5,8,13,21,34,55…)"
+
+print("SEQUÊNCIA DE FIBONACCI")
+print('~='*19)
+
+quant_termos = int(input("Quantos termos você deseja vizualizar? "))
+cont = 1
+anterior = 0 
+
+print(cont)
+
+while cont < quant_termos:
+        cont += 1
+        print(cont, end= '')
+
+        
+
+
+
+
+
