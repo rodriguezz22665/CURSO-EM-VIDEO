65eecb6837cb15aaa47bed5763911874dad9a74e
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-08-20 21:06:21 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_62_super_progress_aritmet.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_62_super_progress_aritmet.py
@@ -1,57 +1,56 @@
 #Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
-
+print("----GERADOR DE PA----\n")
 primeiro = int(input("PRIMEIRO TERMO: "))
 razao = int(input("RAZÃO: "))
 
 cont = 1
 termo = primeiro 
-opcao2 = 0
+cont2 = 0
+total_termos = 10
 
 print('{}'.format(primeiro), end ='')
 
 while cont < 10:
     cont += 1
     termo = termo + razao
-    '''print('{}'.format(primeiro) if cont == 2 else '', end='')'''
     print('' if cont == 1 else ' -> ',termo, end='')
-    print(' -> FIM' if cont == 10 else '', end ='') 
+    print(' -> PAUSA' if cont == 10 else '', end ='')
 
     if cont == 10:
-        opcao = int(input(('\nVocê deseja vizualizar mais termos? Digite [1] para sim ou [0] para sair do programa>: ')))
-        if opcao == 1: 
-            cont = 0
-            while cont < 10: 
-             cont += 1
-             termo = termo + razao
-             print('' if cont == 0 else ' -> ',termo, end='')
-             print(' -> FIM' if cont == 10 else '', end='')
-        elif opcao == 0:
-           print('\033[32mSaindo do programa...\033[m')
-        else:
-           print('\033[31mOPÇÃO INVÁLIDA! Tente novamente!\033[m')
-           opcao2 = int(input(('\nVocê deseja vizualizar mais termos? Digite [1] para sim ou [0] para sair do programa>: ')))
-           if opcao2 == 1: 
-            cont = 0
-            while cont < 10: 
-             cont += 1
-             termo = termo + razao
-             print('' if cont == 0 else ' -> ',termo, end='')
-             print(' -> FIM' if cont == 10 else '', end='')
-           elif opcao2 == 0:
-            print('\033[32mSaindo do programa...\033[m')
-           else:  
-                print('\033[31mOPÇÃO INVÁLIDA! Tente novamente!\033[m')
-                opcao2 = int(input(('\nVocê deseja vizualizar mais termos? Digite [1] para sim ou [0] para sair do programa>: ')))
-                if opcao2 == 1: 
-                    cont = 0
-                while cont < 10: 
-                    cont += 1
-                    termo = termo + razao
-                    print('' if cont == 0 else ' -> ',termo, end='')
-                    print(' -> FIM' if cont == 10 else '', end='')
+        cont2 = int(input(('\nQuantos termos você quer mostrar a mais? ')))
+        while cont2 > 0:
+            cont2 -= 1
+            termo = termo + razao
+            total_termos += 1
+            print(termo, end=' -> ')
+            print('PAUSA' if cont2 == 0 else '', end= '')
+            if cont2 == 0:
+                cont2 = int(input(('\nQuantos termos você quer mostrar a mais? ')))
+print('\nProgressão finalizada com {} termos mostrados.\n'.format(total_termos))
 
-          #AO DIGITAR A OPCAO INVÁLIDA MAIS DE 2 VEZES, ELE ENCERRA O PROGRAMA SEM OFERECER OUTRA OPCAO
-          #PROGRAMA ESTA ENCERRANDO SEM O 0 SER DIGITADO PELO USUÁRIO
+#SOLUCAO PROFESSOR GUANABARA:
+
+'''print('Gerador de PA')
+print('-=' * 10)
+
+primeiro = int(input('PRIMEIRO TERMO: '))
+razao = int(input("RAZÃO: "))
+termo = primeiro
+cont = 1
+mais = 10
+total = 0
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+     print("{} -> ".format(termo), end='')
+     cont += 1
+     termo += razao
+    print('PAUSA')
+    mais = int(input(('Quantos termos você quer mostrar a mais? ')))
+print('\nProgressão finalizada com {} termos mostrados.\n'.format(total))'''
+           
+
+        
                    
     
 
