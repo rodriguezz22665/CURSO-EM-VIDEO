9c218d2d44a5c9e9ec4e38e5c51906ef553175e5
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-08-29 16:49:18 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_66_varios_num_com_flag.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_66_varios_num_com_flag.py
@@ -1,2 +1,13 @@
 #Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 
+cont = soma = 0 
+
+while True:
+    núm = int(input("Digite um valor (999 para parar): "))
+    if núm == 999:
+        break
+    cont += 1
+    soma += núm    
+
+print(f"Você digitou {cont} valores")  
+print(f"A soma de todos os valores foi {soma}")
---
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_67_tabuada_v3.0.py
@@ -0,0 +1,9 @@
+#Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. 
+#O programa será interrompido quando o número solicitado for negativo
+
+print("~^~"*20)
+print('TABOADA V3.0')
+print("~^~"*20)
+
+núm = int(input("\nDigite um número inteiro para vizualizar a sua taboada: "))
+
