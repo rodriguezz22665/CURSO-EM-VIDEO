7203360da54686cee459532c2910c50c359cf3fc
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-02 16:23:29 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_67_tabuada_v3.0.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_67_tabuada_v3.0.py
@@ -1,9 +1,20 @@
 #Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. 
 #O programa será interrompido quando o número solicitado for negativo
+print("="*80)
+print('{:_^80}'.format(' TABOADA V3.0 '))
+print("="*80)
 
-print("~^~"*20)
-print('TABOADA V3.0')
-print("~^~"*20)
+cont = 0
 
-núm = int(input("\nDigite um número inteiro para vizualizar a sua taboada: "))
+núm = int(input("\nDigite um número inteiro para vizualizar a sua taboada: \n[PARA ENCERRAR O PROGRAMA DIGITE UM NÚMERO NEGATIVO] "))
 
+while cont <= 10:
+    cont += 1
+    print(f"{núm} x {cont} = {núm*cont}")
+
+    if cont == 10:
+        núm = int(input("\nDigite um número inteiro para vizualizar a sua taboada: \n[PARA ENCERRAR O PROGRAMA DIGITE UM NÚMERO NEGATIVO] "))
+        cont = 0
+        if núm < 0:
+            break
+print('{:=^80}'.format(" Obrigado por utilizar o TABOADA V3.0!!! "))        
