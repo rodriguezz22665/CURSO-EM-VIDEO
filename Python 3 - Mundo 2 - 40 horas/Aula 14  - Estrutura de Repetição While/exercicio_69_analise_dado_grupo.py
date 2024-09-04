37712f9499a3f5a09ee1e9f5611e93344854c5de
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-04 20:52:24 -0300

    Novos exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_69_analise_dado_grupo.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_69_analise_dado_grupo.py
@@ -3,3 +3,34 @@
 #B) quantos homens foram cadastrados.
 #C) quantas mulheres tem menos de 20 anos.
 
+cont = 0
+A = 0
+B = 0
+C = 0
+print("{:=^80}".format(" CADASTRO DE PESSOAS "))
+
+while True:
+    cont+= 1
+    idade = int(input(f"\nDigite a idade da {cont}º pessoa: "))
+    if idade > 18:
+        A += 1
+    sexo = ' '
+    opcao = ' '
+    while sexo not in 'FM':
+        sexo = str(input(f"Sexo da {cont}ª pessoa [F/M]: ")).strip().upper()[0]
+        if sexo == 'M':
+            B += 1
+        elif sexo == 'F' and idade < 20:
+            C += 1
+    while opcao not in 'SN':    
+        opcao = str(input("Você deseja adicionar mais uma pessoa? [S/N]? ")).strip().upper()[0]
+        if opcao in 'SN':
+            print("~="*40 )
+        
+    if opcao == 'N':
+            break
+    
+print(f"\nForam cadastradas {A} pessoa(s) acima dos 18 anos.")
+print(f"Foram cadastrados {B} homens.")
+print(f"Foram cadastradas {C} mulher(es) com menos de 20 anos.\n")
+print("~="*40)
