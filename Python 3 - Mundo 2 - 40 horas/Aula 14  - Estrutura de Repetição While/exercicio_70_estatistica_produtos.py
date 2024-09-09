0267289a5370e80034ef54e4fbb434bfc00bd797
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-09 16:35:05 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_70_estatistica_produtos.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_70_estatistica_produtos.py
@@ -2,3 +2,50 @@
 #A) qual é o total gasto na compra.
 #B) quantos produtos custam mais de R$1000
 #C) qual é o nome do produto mais barato.           
+
+
+print('{:|^80}'.format(" LOJAS RODRIGUEZz "))
+
+valor_prod = tot_valor = cont = tot_1000 = 0
+mais_barato = ''
+
+while True:
+
+    produto = str(input("\nNOME DO PRODUTO: ")).strip().upper()
+    preco = float(input("PREÇO: "))
+    opcao = str(input("Deseja continuar? [S/N] ")).strip().upper()[0]
+    if opcao in 'S':
+            print("-="*40)
+    cont += 1
+    
+    while opcao not in 'SN':
+        opcao = str(input("Deseja continuar? [S/N] ")).strip().upper()[0]
+        if opcao in 'S':
+            print("-="*40)
+    tot_valor += preco
+
+    if cont == 1:
+        mais_barato = produto
+        valor_prod = preco
+    if valor_prod > preco:
+        valor_prod = preco
+        mais_barato = produto
+        
+
+    if preco > 1000:
+        tot_1000 += 1 
+    if opcao in 'Nn':
+        break
+
+print("-="*40) 
+print('{:=^80}'.format(" RESUMO DA COMPRA "))
+print(f'\nVALOR TOTAL: R$ {tot_valor}')
+print(f'VOCÊ COMPROU {tot_1000} PRODUTO(s) ACIMA DE R$ 1000,00.')
+print(f'{mais_barato} FOI O PRODUTO MAIS BARATO QUE VOCÊ COMPROU.')
+print("\n")
+print('{:^80}'.format("OBRIGADO POR COMPRAR NAS LOJAS RODRIGUEZz! VOLTE SEMPRE!"))    
+print("{:_^80}".format(" © Copyright 2024 "))
+
+
+
+
