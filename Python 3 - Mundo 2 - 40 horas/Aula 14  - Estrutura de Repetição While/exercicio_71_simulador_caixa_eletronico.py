174cc980b56c417d3cbc6195b8fecf2d28b78a8e
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-11 16:49:24 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_71_simulador_caixa_eletronico.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_71_simulador_caixa_eletronico.py
@@ -1,2 +1,18 @@
 #Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
 #considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+print("  /"*27)
+print('{:¤^80}'.format(" CAIXA ELETRÔNICO "))
+print("  /"*27)
+
+saque = int(input("Qual o valor você deseja sacar? "))
+
+cinquenta = saque/50 
+vinte = saque/20
+dez = saque/10
+um = saque/1
+
+print(f'{cinquenta}')
+print(f'{vinte}')
+print(f'{dez}')
+print(f'{um}')
