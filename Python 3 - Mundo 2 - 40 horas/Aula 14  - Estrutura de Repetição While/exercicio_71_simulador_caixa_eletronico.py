62c42ad6930da931ede09c41cf0438b891a3db47
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-12 07:28:16 -0300

    Novos Exercícios

--- Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_71_simulador_caixa_eletronico.py
+++ Python 3 - Mundo 2 - 40 horas/Aula 14  - Estrutura de Repetição While/exercicio_71_simulador_caixa_eletronico.py
@@ -5,13 +5,44 @@ print("  /"*27)
 print('{:¤^80}'.format(" CAIXA ELETRÔNICO "))
 print("  /"*27)
 
+tot_50 = tot_20 = tot_10 = tot_1 = resto = resto2 = 0
 saque = int(input("Qual o valor você deseja sacar? "))
 
-cinquenta = saque/50 
-vinte = saque/20
-dez = saque/10
-um = saque/1
+   
+cinquenta = int(saque/50) 
+vinte = int(saque/20)
+dez = int(saque/10)
+um = int(saque/1)
 
+if saque > 50:
+    tot_50 = cinquenta * 50
+    resto = saque - tot_50
+    print(f"{cinquenta} notas de 50")
+elif saque < 50 and saque >= 20:
+    tot_20 = vinte * 20 
+    resto = saque - tot_20 
+    print(f'{vinte} notas de 20')
+elif saque < 20 and saque >= 10:
+    tot_10 = dez * 10
+    resto = saque - tot_10
+    print(f"{dez} notas de 10")
+elif saque < 10 and saque >= 1:
+    tot_1 = um * 1
+    resto = saque - tot_1     
+    print(f"{um} notas de 1")  
+
+if resto != 0:
+    if resto >= 20:
+        resto2 = resto/20
+        print("notas de 20")
+    elif resto < 20 and resto >= 10:
+        resto2 = resto/10
+        print("notas de 10")
+    elif resto < 10 and resto >= 1:
+        resto2 = resto/1        
+
+print(resto)
+print(resto2)
 print(f'{cinquenta}')
 print(f'{vinte}')
 print(f'{dez}')
