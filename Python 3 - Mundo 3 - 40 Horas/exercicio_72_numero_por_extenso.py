db995c79f286361840d9646a77f6b5c712bb71bc
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-26 22:13:34 -0300

    Novos exercícios

--- Python 3 - Mundo 3 - 40 Horas/exercicio_72_numero_por_extenso.py
+++ Python 3 - Mundo 3 - 40 Horas/exercicio_72_numero_por_extenso.py
@@ -1,2 +1,19 @@
-#Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até inte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+#Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez','onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+'''while True:
+    posicao = int(input('Digite um número entre 0 e 20: '))
+    if posicao < 0 or posicao > 20:
+        print("Número inválido! Tente novamente.")
+    else:    
+     print(f'Você digitou o número {extenso[posicao]}.')
+     break'''
+
+#Solucao professor Guanabara
+while True:
+    posicao = int(input('Digite um número entre 0 e 20: '))  
+    if 0 <= posicao <= 20:
+       break  
+    print("Número inválido! Tente novamente.", end='')
+print(f'Você digitou o número {extenso[posicao]}.')    
