c0b52827ebc6ed9a88850557f7350bb793fd4b0b
Rodrigo de Souza Rodrigues <rodrigo.rodriguez.u2.2017@gmail.com>
2024-09-28 11:36:28 -0300

    Novos exercícios

--- Python 3 - Mundo 3 - 40 Horas/exercicio_73_tuplas_com_time-futeb.py
+++ Python 3 - Mundo 3 - 40 Horas/exercicio_73_tuplas_com_time-futeb.py
@@ -2,4 +2,21 @@
 '''a) Os 5 primeiros times.
 b) Os últimos 4 colocados.
 c) Times em ordem alfabética.
-d) Em que posição está o time da Chapecoense'''
+d) Em que posição está o time da Cruzeiro'''
+
+print('{:=^80}'.format(" TABELA DO CAMPEONATO BRASILEIRO "))
+
+tabela = ('Botafogo','Palmeiras','Fortaleza','Flamengo',
+          'São Paulo','Bahia','Cruzeiro','Internacional','Atlético-MG','Vasco','Bragantino','Juventude','Criciúma','Grêmio','Athletico-PR','Vitória','Corinthians','Fluminense','Cuiabá','Atlético-GO')
+
+print('='*80)
+print(f'Lista dos times do Brasileirão: {tabela}')
+print('='*80)
+print(f'Os 5 primeiros colocados são: {tabela[:5]}')
+print('='*80)
+print(f'Os 4 últimos colocados são: {tabela[16:]}')
+print('='*80)
+print(f'Times em ordem alfabética: {sorted(tabela)}')
+print('='*80)
+print(f'O Cruzeiro está na {tabela.index("Cruzeiro")+1}º posição.')
+print('='*80)
